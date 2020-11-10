55467e27ef3dd98a282346a213c7d317063b3d45
Yoharu <jonathas_a.a@hotmail.com>
2020-11-10 18:25:41 -0300

    Cria notificação de attachments para job Feita a criação da notificação de attachments para job, quando alguém se aplica a vaga.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Job extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'title',
@@ -40,5 +42,5 @@ class Job extends Model
     {
         return $this->belongsTo(Type::class);
     }
-    
+
 }
